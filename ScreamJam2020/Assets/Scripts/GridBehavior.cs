d2b4e5028b976b9cf1877bacc6e6365e76e42aa2
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2020-10-16 15:13:44 -0700

    Update with scene
    Scene has begun sculpting. Player can walk around and colliders keep him on a grid. Monster still needs to obey colliders.

--- ScreamJam2020/Assets/Scripts/GridBehavior.cs
+++ ScreamJam2020/Assets/Scripts/GridBehavior.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class GridBehavior : MonoBehaviour
 {
 
+    [SerializeField] private GameObject player, target;
     [SerializeField] private float gridSize = 1;
+    [SerializeField] private float heightFromGround;
+    [SerializeField] private float cameraXMin, cameraXMax, cameraYMin, cameraYMax, cameraSpeedX, cameraSpeedY;
+    private float cameraX, cameraY;
     private GameObject[] enemies;
 
     // Start is called before the first frame update
@@ -17,6 +21,7 @@ public class GridBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CameraRotation();
         GridMovement();
     }
 
@@ -24,24 +29,33 @@ public class GridBehavior : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) && ValidMoveForward())
         {
-            transform.position += transform.forward * gridSize;
+            player.transform.position += player.transform.forward * gridSize;
+            setHeightFromGround();
             MakeMonsterMove();
         }
 
         if (Input.GetKeyDown(KeyCode.S) && ValidMoveBackward())
         {
-            transform.position -= transform.forward * gridSize;
+            player.transform.position -= player.transform.forward * gridSize;
+            setHeightFromGround();
             MakeMonsterMove();
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.rotation *= Quaternion.Euler(0, -90, 0);
+            transform.LookAt(target.transform, Vector3.up);
+            player.transform.rotation *= Quaternion.Euler(0, -90, 0);
+            cameraXMin -= 90;
+            cameraXMax -= 90;
+            transform.LookAt(target.transform, Vector3.up);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.rotation *= Quaternion.Euler(0, 90, 0);
+            player.transform.rotation *= Quaternion.Euler(0, 90, 0);
+            cameraXMin += 90;
+            cameraXMax += 90;
+            transform.LookAt(target.transform, Vector3.up);
         }
     }
 
@@ -49,7 +63,7 @@ public class GridBehavior : MonoBehaviour
     {
         Debug.DrawRay(transform.position, transform.forward, Color.green);
         Debug.Log("Something is in front of the player: " + Physics.Raycast(transform.position, transform.forward, gridSize));
-        if (Physics.Raycast(transform.position, transform.forward, gridSize))
+        if (Physics.Raycast(player.transform.position, player.transform.forward, gridSize))
         {
             Debug.Log("Not a valid move.");
             return false;
@@ -65,7 +79,7 @@ public class GridBehavior : MonoBehaviour
     {
         Debug.DrawRay(transform.position, -transform.forward, Color.green);
         Debug.Log("Something is in front of the player: " + Physics.Raycast(transform.position, -transform.forward, gridSize));
-        if (Physics.Raycast(transform.position, -transform.forward, gridSize))
+        if (Physics.Raycast(player.transform.position, -player.transform.forward, gridSize))
         {
             Debug.Log("Not a valid move.");
             return false;
@@ -86,5 +100,28 @@ public class GridBehavior : MonoBehaviour
             enemies[i].gameObject.GetComponent<MonsterGridBehavior>().MonsterMovement();
             Debug.Log("Monster moved.");
         }
+    }
+
+    private void setHeightFromGround()
+    {
+        RaycastHit hit;
+
+        Ray heightRay = new Ray(transform.position, Vector3.down);
+        
+        if(Physics.Raycast(heightRay, out hit))
+        {
+            float hoverError = heightFromGround - hit.distance;
+            player.transform.position += new Vector3(0, hoverError, 0);
+        }
+    }
+
+    private void CameraRotation()
+    {
+        cameraX += cameraSpeedX * Input.GetAxis("Mouse X");
+        cameraY -= cameraSpeedY * Input.GetAxis("Mouse Y");
+        cameraY = Mathf.Clamp(cameraY, cameraYMin, cameraYMax);
+        cameraX = Mathf.Clamp(cameraX, cameraXMin, cameraXMax);
+        transform.eulerAngles = new Vector3(0, cameraX, 0.0f);
+        transform.eulerAngles = new Vector3(cameraY, cameraX, 0);
     }
 }
--- ScreamJam2020/Assets/Scripts/MonsterGridBehavior.cs
+++ ScreamJam2020/Assets/Scripts/MonsterGridBehavior.cs
@@ -6,15 +6,17 @@ public class MonsterGridBehavior : MonoBehaviour
 {
 
     [SerializeField] private GameObject player;
+    [SerializeField] private float heightFromGround;
     private Vector3 directionToPlayer;
     private float xDirectionSign;
     private float zDirectionSign;
+    private int speed = 1;
 
     public void MonsterMovement()
     {
         //Find vector between player and monster, ask whether x and z directions are each positive or negative.
         //Verticality does not matter, so ignore y component.
-        directionToPlayer = transform.position - player.transform.position;
+        directionToPlayer = (transform.position - player.transform.position).normalized;
         xDirectionSign = directionToPlayer.x / Mathf.Abs(directionToPlayer.x);
         zDirectionSign = directionToPlayer.z / Mathf.Abs(directionToPlayer.z);
 
@@ -22,12 +24,28 @@ public class MonsterGridBehavior : MonoBehaviour
         if (Mathf.Abs(directionToPlayer.x) >= Mathf.Abs(directionToPlayer.z))
         {
             //Move monster one space along grid in the x direction toward player
-            transform.position += new Vector3(1, 0, 0) * -xDirectionSign;
+            transform.position += new Vector3(speed, 0, 0) * -xDirectionSign;
         }
         else
         {
             //Move monster one space along grid in the z direction toward player
-            transform.position += new Vector3(0, 0, 1) * -zDirectionSign;
+            transform.position += new Vector3(0, 0, speed) * -zDirectionSign;
+        }
+
+        setHeightFromGround();
+        transform.LookAt(player.transform, Vector3.up);
+    }
+
+    private void setHeightFromGround()
+    {
+        RaycastHit hit;
+
+        Ray heightRay = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(heightRay, out hit))
+        {
+            float hoverError = heightFromGround - hit.distance;
+            transform.position += new Vector3(0, hoverError, 0);
         }
     }
 }
